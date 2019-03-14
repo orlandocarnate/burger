4fdb861bae40665f87e448bdc38ff47174656605
Orlando Carnate <orlando.carnate@gmail.com>
2019-03-14 00:54:59 -0500

    Design index with bootstrap and custom css and fonts

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,6 +4,10 @@ var express = require("express");
 // create router object
 var router = express.Router();
 
+// Main Root directory.
+router.get("/", function (req, res) {
+    res.render("index");
+});
 
 // export router object for server.js
 module.exports = router;
